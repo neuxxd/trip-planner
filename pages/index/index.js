60d8719242ce07c905949ae1c841c8f4652ff5e1
TripPlanner Dev <dev@trip-planner.com>
2026-03-28 16:10:24 +0800

    chore: 删除风火轮API调用

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,32 +7,6 @@ Page({
 
   onLoad: function() {
     this.loadRecentTrips();
-    this.fetchRecommendedSpots();
-  },
-
-  // 调用风火轮API获取推荐景点
-  fetchRecommendedSpots: function() {
-    // 注意：需要在微信小程序后台添加 https://api.fenghuolun.vip 到 request 合法域名
-    // 临时注释，等待域名配置完成
-    /*
-    const baseUrl = 'https://api.fenghuolun.vip';
-    wx.request({
-      url: baseUrl + '/api/spots/recommend',
-      method: 'GET',
-      data: {
-        appid: 'appid123'
-      },
-      success: (res) => {
-        if (res.data && res.data.code === 0) {
-          console.log('推荐景点获取成功', res.data.data);
-        }
-      },
-      fail: (err) => {
-        console.error('推荐景点获取失败', err);
-      }
-    });
-    */
-    console.log('推荐景点API已禁用，请在微信小程序后台添加 api.fenghuolun.vip 到合法域名');
   },
 
   onShow: function() {
